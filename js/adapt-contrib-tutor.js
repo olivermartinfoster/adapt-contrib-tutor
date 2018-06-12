35d1f0daa49088fc7722a4112b4175f30e3c69d9
Guy Willis <guy.willis@kineo.com>
2018-06-12 15:43:49 +0100

    Added notify prefix

--- js/adapt-contrib-tutor.js
+++ js/adapt-contrib-tutor.js
@@ -15,26 +15,26 @@ define([
     if (view.model.has('_isCorrect')) {
       // Attach specific classes so that feedback can be styled.
       if (view.model.get('_isCorrect')) {
-        classes.push('correct');
+        classes.push('notify--correct');
       } else {
         if (view.model.has('_isAtLeastOneCorrectSelection')) {
           // Partially correct feedback is an option.
           if (view.model.get('_isAtLeastOneCorrectSelection')) {
-            classes.push('partially-correct');
+            classes.push('notify--partially-correct');
           } else {
-            classes.push('incorrect');
+            classes.push('notify--incorrect');
           }
         } else {
-          classes.push('incorrect');
+          classes.push('notify--incorrect');
         }
       }
     }
 
     // Add the extension/component type which triggered this.
     if (view.model.has('_component')) {
-      classes.push('component-' + view.model.get('_component'));
+      classes.push('notify--component-' + view.model.get('_component'));
     } else if (view.model.has('_extension')) {
-      classes.push('extension-' + view.model.get('_extension'));
+      classes.push('notify--extension-' + view.model.get('_extension'));
     }
 
     // Add the _id property as attribute.
